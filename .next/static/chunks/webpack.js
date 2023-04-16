e783e4f4be562984aee8a5a333cc3ec3a50eb94b
jinow9595 <jinow9595@gmail.com>
2023-04-16 14:05:08 +0900

    불필요한 파일 삭제

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b7fe743a6360ee2"; }
+/******/ 		__webpack_require__.h = function() { return "ef22689368dd336f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Main() {
   return (
     <>
       <div>Hello world!</div>
